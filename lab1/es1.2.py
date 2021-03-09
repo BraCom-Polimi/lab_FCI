b65f4547bd67096f1c15cd8582fc0c016c455eab
Matteo Chen <kaiximatteo.chen@mail.polimi.it>
2021-03-09 17:44:40 +0100

    upload

--- lab1/es1.2.py
+++ lab1/es1.2.py
@@ -1,4 +1,4 @@
-mport requests
+import requests
 
 def calcAvg(list):
    return sum(list)/len(list)
@@ -19,26 +19,5 @@ for url in sites:
 
 for element in forVisual:
    print (element)
-
-print ("winner: ", sites[avg.index(min(avg))])import requests
-def calcAvg(list):
-    return sum(list)/len(list)
-
-sites = ["http://www.google.com", "http://www.youtube.com", "http://www.facebook.com", "http://www.bloomberg.com", "http://www.netflix.com", "http://binance.com"]
-
-times = []
-avg = []
-forVisual = []
-
-for url in sites:
-    for _ in range(10):
-        r = requests.get(url)
-        times.append(r.elapsed.microseconds/1000)
-    avg.append(calcAvg(times))
-    forVisual.append(url)
-    forVisual.append(calcAvg(times))
-
-for element in forVisual:
-    print (element)
 
 print ("winner: ", sites[avg.index(min(avg))])
